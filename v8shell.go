48a122dc880515e00b80c41283e08828d7aaf3f4
Heuristic Fencepost <h.fencepost@gmail.com>
2011-06-15 23:05:35 -0500

    Some simple refactoring, mainly creating distinct make targets for the wrapper and shell.

--- v8shell.go
+++ v8shell.go
@@ -5,6 +5,8 @@ import v8 "v8"
 import io "io/ioutil"
 import flag "flag"
 
+// Simple shell for running arbitrary JavaScript code.  Assumes that the JavaScript file in question returns
+// a string result.
 func main() {
 
 	var scriptname *string = flag.String("script","v8.js","Name of JavaScript source file to run")
@@ -13,8 +15,6 @@ func main() {
 	fmt.Printf("Using JavaScript file: %s\n",*scriptname)
 	bytes,err := io.ReadFile(*scriptname)
 	if err == nil {
-
-		fmt.Printf("Script: %s\n",bytes)	
 
 		var r string = v8.JsRuntimeExecute(string(bytes))
 		fmt.Printf("Result: %s\n",r)
